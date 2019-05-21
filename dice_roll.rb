33c8fdf9f02f16ce21936f8bed8494599b7480c0
Becky Phan <beckyphan@live.com>
2019-05-21 17:52:39 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,6 +1,7 @@
 #using rand()
 #rand(1..6) returns a random integer between 1-6 inclusive
 #rand(1...6) returns a random integer between 1 inclusive, through 6 exclusive
+
 def roll()
   return rand(1..6)
 end
